77db12fece6629f51c55012282f4ac299f8223e4
Eli Holmes <eli.holmes@noaa.gov>
2022-03-30 15:02:21 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 import os
 import sys
 # Need this so sphinx can find lumache.py. Change is .py files are elsewhere than root.
-sys.path.insert(0, os.path.abspath('../..'))
+# sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../src'))
 
 # -- Project information
