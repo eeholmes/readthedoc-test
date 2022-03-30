1060ea10651003a6ea9db2285a82b3fc52bfc0d4
Eli Holmes <eli.holmes@noaa.gov>
2022-03-30 15:07:10 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,13 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinxcontrib.apidoc',
 ]
+
+apidoc_module_dir = '../src'
+apidoc_output_dir = 'source'
+apidoc_excluded_paths = ['tests']
+apidoc_separate_modules = False
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
