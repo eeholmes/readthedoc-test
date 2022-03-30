3f56013bf64626fada82daa00546f323df32ac09
Eli Holmes <eli.holmes@noaa.gov>
2022-03-30 12:51:41 -0700

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -21,3 +21,13 @@ def get_random_ingredients(kind=None):
     :rtype: list[str]
     """
     return ["shells", "gorgonzola", "parsley"]
+
+def another_function(kind=None):
+    """
+    Return a list of random ingredients as strings.
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :return: The ingredients list.
+    :rtype: list[str]
+    """
+    return ["shells", "gorgonzola", "parsley"]
