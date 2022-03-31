9a1c67f8f257cdde3dd22652e74698bede0daddb
Eli Holmes <eli.holmes@noaa.gov>
2022-03-30 20:52:22 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@ sys.path.insert(0, os.path.abspath('../../src'))
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Coastal'
+copyright = '2021'
+author = 'Multiple'
 
 release = '0.1'
 version = '0.1.0'
@@ -30,9 +30,9 @@ autoapi_type = 'python'
 autoapi_dirs = ['../../src']
 
 #apidoc_module_dir = '../../src'
-#apidoc_output_dir = 'source'
+#apidoc_output_dir = '.'
 #apidoc_excluded_paths = ['tests']
-#apidoc_separate_modules = False
+#apidoc_separate_modules = True
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
@@ -48,3 +48,35 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_repo": "eeholmes/readthedoc-test", # Repo name
+    "github_version": "main", # Version
+    "conf_py_path": "docs/source/", # Path in the checkout to the docs root
+}
+
+# -- Options for HTML output -------------------------------------------------
+
+html_short_title = "topobathy"
+html_show_sourcelink = False
+html_show_sphinx = True
+html_show_copyright = True
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+repository_url = f"https://github.com/ICESAT-2HackWeek/coastal_topobathy"
+html_context = {
+    "menu_links": [
+        (
+            '<i class="fa fa-github fa-fw"></i> Source Code',
+            repository_url,
+        ),
+        (
+            '<i class="fa fa-book fa-fw"></i> License',
+            f"{repository_url}/blob/main/LICENSE",
+        ),
+    ],
+}
