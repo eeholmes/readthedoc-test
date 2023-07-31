f4baea5a735ee976a2fe20ea38f721272809add2
AaronW <aaron.whang@realvision.com>
2023-07-31 12:12:29 -0400

    test

--- lumache.py
+++ lumache.py
@@ -6,7 +6,7 @@ __version__ = "0.1.0"
 
 
 class InvalidKindError(Exception):
-    """Raised if the kind is invalid. and lel"""
+    """Raised if the kind is invalid. and lesetter"""
     pass
 
 def first_function(kind=None):
